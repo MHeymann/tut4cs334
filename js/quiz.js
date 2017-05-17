85c1d627a2acdc00a3b118f1a467422f043282a4
Murray Heymann <heymann.murray@gmail.com>
2017-05-17 15:28:42 +0200

    Receive and display multiple choice qs

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,6 @@
+var questions = null;
+
 $(document).ready(function() {
-	console.log("Whaaahahaha");
 	$.ajax({
 		url: "php/get_qs.php",
 		async: false,
@@ -13,11 +14,69 @@ $(document).ready(function() {
 			console.log(errorMessage);
 		}
 	});
-	console.log("Whaaaheeehehehehe");
 
 });
 
 function handle_get_qs_response(response) {
 	//console.log(response);
-	console.log(JSON.stringify(response));
+	if (response.error) {
+		console.log(response.error_message);
+	} else  {
+		console.log(JSON.stringify(response));
+		questions = response.questions;
+		set_questions_to_html();
+	}
+
+}
+
+function set_questions_to_html() {
+	var $multi_div = $("#multi-div");
+	var $written_div = $("#written-div");
+	var overall_count = 0;
+	if (questions == null) {
+		console.log("No questoins set!");
+		return;
+	}
+
+	$multi_div.empty();
+
+	for (var i = 0; i < questions.multi.length; i++) {
+		overall_count++;
+		var $panel_inner = $("<div/>", {
+			"class": "panel-body"
+		});
+		var $panel_outer = $("<div/>", {
+			"class": "panel panel-default",
+			html: $panel_inner
+		});
+		$panel_inner.html($("<label/>", {
+			text: "question " + overall_count + ":"
+		}));
+		$panel_inner.append("<br>");
+		$panel_inner.append($("<label/>", {
+			text: questions.multi[i].question
+		}));
+
+		for (var j = 0; j < questions.multi[i].opts.length; j++) {
+			var $radio = $("<input/>", {
+				type: "radio",
+				name: questions.multi[i].qID,
+				value: questions.multi[i]['opts'][j]
+			});
+			var $label = $("<label/>", {
+				text: questions.multi[i]['opts'][j]
+			});
+			var $div = $("<div/>", {
+				"class": "radio"
+			});
+
+			$label.prepend($radio);
+			$div.html($label);
+			
+			$panel_inner.append($div);
+		}
+
+		//$panel_outer.append($panel_inner);
+		$multi_div.append($panel_outer);
+	}
 }
--- php/get_qs.php
+++ php/get_qs.php
@@ -9,13 +9,45 @@ $email   = $_POST['user'];
  * TODO: connect to mysql database, check user password.
  */
 
+$multi = array();
+$written = array();
+for ($i = 1; $i <= 5; $i++) {
+	$multi[($i-1)] = array();
+	$written[($i-1)] = array();
+	$multi[($i-1)]["qID"] = $i + 509;
+	$written[($i-1)]["qID"] = $i + 403;
+	$multi[($i-1)]["question"] = "some question, multi number $i";
+	$written[($i-1)]["question"] = "some question, written number $i";
+
+	$multi[($i-1)]["opts"] = array();
+	$multi[($i-1)]["opts"][0] = "some option 1 for question $i";
+	$multi[($i-1)]["opts"][1] = "some option 2 for question $i";
+	$multi[($i-1)]["opts"][2] = "some option 3 for question $i";
+	$multi[($i-1)]["opts"][3] = "some option 4 for question $i";
+
+	$multi[($i-1)]["answer"] = "answer for multiple choice question number $i";
+	$written[($i-1)]["answer"] = "answer for written question number $i";
+}
+
+
+
 $json = array(
 	"error" => false,
 	"user" => $email,
 	"questions" => array(
-		"q" => "Liewe heksie"
+		"multi" => $multi,
+		"written" => $written
 	)
 );
 
 echo json_encode($json);
+
+
+function echo_error($error_message) {
+	echo json_encode(array(
+		"error" => true,
+		"error_message" => $error_message
+	));
+	die();
+}
 ?>
